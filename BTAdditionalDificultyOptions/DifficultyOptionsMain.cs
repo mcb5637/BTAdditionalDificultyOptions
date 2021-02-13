027d2fdd109e2c63d98b88806b5c82d73e87ce91
mgrbo <mgrbo@web.de>
2021-02-13 16:50:19 +0100

    Add project files.

---
+++ BTAdditionalDificultyOptions/DifficultyOptionsMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BattleTech;
+using Harmony;
+
+[assembly:AssemblyVersion("0.1.0.0")]
+
+namespace BTAdditionalDificultyOptions
+{
+    class DifficultyOptionsMain
+    {
+        public static Action<SimGameState, string, string, string> ApplySettings;
+
+        public static void Init(string directory, string settingsJSON)
+        {
+            ApplySettings += ApplyTags;
+            HarmonyInstance harmony = HarmonyInstance.Create("com.github.mcb5637.BTAdditionalDificultyOptions");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        private static void ApplyTags(SimGameState s, string id, string key, string val)
+        {
+            //FileLog.Log(key + " " + val);
+            if (key.Equals("AddSimGameTag"))
+                s.CompanyTags.Add(val);
+            if (key.Equals("RemoveSimGameTag") && s.CompanyTags.Contains(val))
+                s.CompanyTags.Remove(val);
+        }
+    }
+}
---
+++ BTAdditionalDificultyOptions/SimGameDifficulty_ApplySetting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleTech;
+using Harmony;
+
+namespace BTAdditionalDificultyOptions
+{
+    [HarmonyPatch(typeof(SimGameDifficulty), "ApplySetting")]
+    class SimGameDifficulty_ApplySetting
+    {
+        public static void Prefix(SimGameDifficulty __instance, SimGameState ___simState, SimGameDifficulty.DifficultySetting setting, int index, ref List<SimGameDifficulty.DifficultyConstantValue> __state)
+        {
+            __state = null;
+            if (setting == null)
+			{
+				return;
+			}
+            SimGameDifficulty.DifficultyOption opt = setting.Options[index];
+            if (opt.DifficultyConstants != null)
+            {
+                __state = opt.DifficultyConstants;
+                foreach (SimGameDifficulty.DifficultyConstantValue v in opt.DifficultyConstants)
+                {
+                    if (v.ConstantType.Equals("AdditionalSettings"))
+                    {
+                        DifficultyOptionsMain.ApplySettings(___simState, setting.ID, v.ConstantName, v.ConstantValue);
+                    }
+                }
+                opt.DifficultyConstants = opt.DifficultyConstants.Where((x) => !x.ConstantType.Equals("AdditionalSettings")).ToList();
+            }
+        }
+
+        public static void Postfix(SimGameDifficulty.DifficultySetting setting, int index, List<SimGameDifficulty.DifficultyConstantValue> __state)
+        {
+            if (__state != null)
+                setting.Options[index].DifficultyConstants = __state;
+        }
+    }
+}
